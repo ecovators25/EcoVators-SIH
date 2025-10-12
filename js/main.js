d3b11bdb5850bae07e0cbd8a0225f29db99f5c71
ecovators25 <ecovators.sih@gmail.com>
2025-10-12 22:19:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 window.location.href = 'htmls/teacher.html';
                 break;
             case 'admin':
-                window.location.href = 'htmls/Admin.html';
+                window.location.href = 'htmls/CooperativeSociety.html';
                 break;
             case 'company':
                 window.location.href = 'htmls/Company.html';
